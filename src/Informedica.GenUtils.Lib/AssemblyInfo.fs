88bf12a2729b170169c859fda56c764c72acdd27
Casper Bollen <halcwb@gmail.com>
2016-04-30 10:17:10 +0200

    Added src files and chanched .gitignore to match project

---
+++ src/Informedica.GenUtils.Lib/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Informedica.GenUtils.Lib")>]
+[<assembly: AssemblyProductAttribute("GenUtils")>]
+[<assembly: AssemblyDescriptionAttribute("")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
