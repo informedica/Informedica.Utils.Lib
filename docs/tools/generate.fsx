1c3cc015125aca968ea7d678d141e4f929a89d39
Casper Bollen <halcwb@gmail.com>
2016-05-13 16:25:45 +0200

    Fixed wrong project name

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -12,9 +12,9 @@
 // for binaries output to root bin folder please add the filename only to the 
 // referenceBinaries list below in order to generate documentation for the binaries.
 // (This is the original behaviour of ProjectScaffold prior to multi project support)
-let projectName = Settings.project
+let projectName = Settings.gitName
 let gitHome = Settings.gitOwner
-let gitLink = Settings.gitName
+let gitLink = Settings.reposName
 let summary = Settings.summary
 let author  = Settings.gitOwner
 
