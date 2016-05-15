b5868db1d9dc0c4b0ee37869ab6af9ba3af0cbc7
Casper Bollen <halcwb@gmail.com>
2016-05-15 15:00:38 +0200

    Updated assembly info

--- src/Informedica.GenUtils.Lib/AssemblyInfo.fs
+++ src/Informedica.GenUtils.Lib/AssemblyInfo.fs
@@ -5,9 +5,9 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Informedica.GenUtils.Lib")>]
 [<assembly: AssemblyCompanyAttribute("halcwb")>]
 [<assembly: AssemblyDescriptionAttribute("A library with utility functions")>]
-[<assembly: AssemblyVersionAttribute("0.1.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
+[<assembly: AssemblyVersionAttribute("0.2.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.1"
+    let [<Literal>] Version = "0.2.1"
