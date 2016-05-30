36150385bfabf7035f1f3bf2a3cadc5dab6fea86
Casper Bollen <halcwb@gmail.com>
2016-05-30 23:52:52 +0200

    Added Reflection module

--- src/Informedica.GenUtils.Lib/Scripts/load-project-release.fsx
+++ src/Informedica.GenUtils.Lib/Scripts/load-project-release.fsx
@@ -1,5 +1,6 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
 #load "load-references-release.fsx"
-#load "../String.fs"
+#load "../Reflection.fs"
+      "../String.fs"
       "../BigRational.fs"
