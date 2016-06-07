220b6ed8f64de8f3d976ae69e28a0cbd416088d2
Casper Bollen <halcwb@gmail.com>
2016-06-07 21:14:42 +0200

    Modified dependencies and added app.config to project

--- src/Informedica.GenUtils.Lib/Scripts/load-project-release.fsx
+++ src/Informedica.GenUtils.Lib/Scripts/load-project-release.fsx
@@ -1,6 +1,9 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
 #load "load-references-release.fsx"
-#load "../Reflection.fs"
+#load "../NullCheck.fs"
+      "../List.fs"
+      "../Option.fs"
+      "../Reflection.fs"
       "../String.fs"
       "../BigRational.fs"
