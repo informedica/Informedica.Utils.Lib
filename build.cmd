f67a986beb02539e3ee3d6e16580666af2768254
halcwb <halcwb@gmail.com>
2019-05-16 21:18:53 +0200

    chore: improved bootstrapping build

--- build.cmd
+++ build.cmd
@@ -1,16 +1,6 @@
 @echo off
 cls
-::Only bootstrap when no paket.exe
-if not exist .paket\paket.exe (
-	.paket\paket.bootstrapper.exe
-	if errorlevel 1 (
-	  exit /b %errorlevel%
-	)
-)
-::Update dependencies
-.paket\paket.exe restore
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-::Build the project
-packages\FAKE\tools\FAKE.exe build.fsx %*
+
+dotnet restore build.proj
+
+fake build --target %*
--- build.sh
+++ build.sh
@@ -1,40 +1,12 @@
 #!/usr/bin/env bash
+# to properly set Travis permissions: https://stackoverflow.com/questions/33820638/travis-yml-gradlew-permission-denied
+# git update-index --chmod=+x fake.sh
+# git commit -m "permission access for travis"
 
 set -eu
 set -o pipefail
 
-cd `dirname $0`
+dotnet restore build.proj
 
-FSIARGS=""
-OS=${OS:-"unknown"}
-if [[ "$OS" != "Windows_NT" ]]
-then
-  FSIARGS="--fsiargs -d:MONO"
-fi
 
-function run() {
-  if [[ "$OS" != "Windows_NT" ]]
-  then
-    mono "$@"
-  else
-    "$@"
-  fi
-}
-#Only run the bootstrapper if no paket.exe
-if [ ! -e .paket/paket.exe ]
-	then
-		run .paket/paket.bootstrapper.exe
-fi
-
-if [[ "$OS" != "Windows_NT" ]] &&
-       [ ! -e ~/.config/.mono/certs ]
-then
-  mozroots --import --sync --quiet
-fi
-
-run .paket/paket.exe restore
-
-[ ! -e build.fsx ] && run .paket/paket.exe update
-
-run packages/FAKE/tools/FAKE.exe "$@" $FSIARGS build.fsx
-
+fake build --target $@
