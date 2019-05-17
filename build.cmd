950e95cdf766d9f1d4964fd3c6d3f30def6a37a8
halcwb <halcwb@gmail.com>
2019-05-17 17:44:25 +0200

    fix: trying to fix CI builds

--- build.cmd
+++ build.cmd
@@ -1,6 +1,14 @@
 @echo off
 cls
 
+::Only bootstrap when no paket.exe
+if not exist .paket\paket.exe (
+	.paket\paket.bootstrapper.exe
+	if errorlevel 1 (
+	  exit /b %errorlevel%
+	)
+)
+
 dotnet restore build.proj
 
 fake build --target %*
--- build.sh
+++ build.sh
@@ -6,6 +6,23 @@
 set -eu
 set -o pipefail
 
+OS=${OS:-"unknown"}
+
+function run() {
+  if [[ "$OS" != "Windows_NT" ]]
+  then
+    mono "$@"
+  else
+    "$@"
+  fi
+}
+
+#Only run the bootstrapper if no paket.exe
+if [ ! -e .paket/paket.exe ]
+	then
+		run .paket/paket.bootstrapper.exe
+fi
+
 dotnet restore build.proj
 
 
