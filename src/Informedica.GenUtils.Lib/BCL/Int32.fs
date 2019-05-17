124a1d4177eef40eaf7d7efd4b8abd205e9992d4
halcwb <halcwb@gmail.com>
2019-05-17 14:35:53 +0200

    fix: forgat to add BCL folder

---
+++ src/Informedica.GenUtils.Lib/BCL/Int32.fs
@@ -0,0 +1,11 @@
+﻿namespace Informedica.GenUtils.Lib.BCL
+    
+module Int32 =
+
+    open System
+
+    let parse s = Int32.Parse(s, Globalization.NumberStyles.Any, Globalization.CultureInfo.InvariantCulture)
+
+    let tryParse s =
+        let (b, n) = Int32.TryParse(s, Globalization.NumberStyles.Any, Globalization.CultureInfo.InvariantCulture)
+        if b then n |> Some else None
