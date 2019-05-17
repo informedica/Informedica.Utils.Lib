ef78863236c01ec908a10be0750be28347bcc03a
halcwb <halcwb@gmail.com>
2019-05-17 13:26:10 +0200

    chore: added BCL modules

--- src/Informedica.GenUtils.Lib/NullCheck.fs
+++ src/Informedica.GenUtils.Lib/NullCheck.fs
@@ -3,12 +3,15 @@
 /// Function to perform a safe null check
 module NullCheck =
 
-    /// This is the F# 4 implementation of
-    /// checking whether a value is null.
-    [<CompiledName("IsNull")>]
-    let inline isNull (value : 'T) = 
-        match value with 
-        | null -> true 
-        | _ -> false
+    let nullOrDef f d v =
+        if v |> isNull then d
+        else v |> f
 
-
+    let nullOrDef2 f d v1 v2 =
+        if (v1 |> isNull) || (v2 |> isNull) then d
+        else f v1 v2
+        
+    let nullOrDef3 f d v1 v2 v3 =
+        if (v1 |> isNull) || (v2 |> isNull) || (v2 |> isNull) then d
+        else f v1 v2 v3
+        
--- src/Informedica.GenUtils.Lib/Scripts/Script.fsx
+++ src/Informedica.GenUtils.Lib/Scripts/Script.fsx
@@ -1,8 +1,46 @@
-﻿#load "load-project-release.fsx"
+﻿// First load all dependencies
+#r "netstandard"
 
-open Informedica.GenUtils.Lib
+#load "../../../.paket/load/netstandard2.1/main.group.fsx"
+#load "../../../.paket/load/netstandard2.1/Testing/testing.group.fsx"
 
-type Test = |Pass | Fail
+#load "../BCL/Char.fs"
+#load "../Reflection.fs"
 
-Reflection.toString Pass
-Reflection.fromString<Test> "blah"
+
+
+module CharTests =
+
+    open Informedica.GenUtils.Lib.BCL
+    open Swensen.Unquote
+
+    // all lower case letters are not capitals
+    test
+        <@
+            Char.letters
+            |> Seq.forall (fun c ->
+                c |> (Char.isCapital >> not)
+            ) 
+        @>
+
+    // all capitals are capitals
+    test 
+        <@
+            Char.capitals
+            |> Seq.forall (fun c ->
+                c |> Char.isCapital
+            ) 
+        @>
+
+    
+
+
+module ReflectionTests =
+
+    open Informedica.GenUtils.Lib
+    
+    type Test = |Pass | Fail
+
+    Reflection.toString Pass
+    Reflection.fromString<Test> "blah"
+    Reflection.fromString<Test> "Pass"
